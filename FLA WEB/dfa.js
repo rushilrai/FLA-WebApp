e249002e2702f20b79208bd415f6dadda543fac3
rabhyasighat <66615638+rabhyasighat@users.noreply.github.com>
2020-09-28 21:36:22 +0530

    best of luck nikki

---
+++ FLA WEB/dfa.js
@@ -0,0 +1,15 @@
+function createNewElement() {
+    var txtNewInputBox = document.createElement('div');
+    txtNewInputBox.innerHTML = "<input type='text' id='newInputBox'>";
+    document.getElementById("newElementId").appendChild(txtNewInputBox);
+}
+function createNewElement2() {
+    var txtNewInputBox2 = document.createElement('div');
+    txtNewInputBox.innerHTML = "<input type='text' id='newInputBox2'>";
+    document.getElementById("newElementId2").appendChild(txtNewInputBox2);
+}
+function createNewElement3() {
+    var txtNewInputBox3 = document.createElement('div');
+    txtNewInputBox.innerHTML = "<input type='text' id='newInputBox3'>";
+    document.getElementById("newElementId3").appendChild(txtNewInputBox3);
+}
