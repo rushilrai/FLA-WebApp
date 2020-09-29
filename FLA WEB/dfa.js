98b8a73b19821eb1966b1028b442d65ec59b2252
rabhyasighat <66615638+rabhyasighat@users.noreply.github.com>
2020-09-29 08:48:16 +0530

    .

--- FLA WEB/dfa.js
+++ FLA WEB/dfa.js
@@ -1,17 +1,34 @@
-function addInput() {
-    inputs.push(document.getElementById('input').value);
-    document.getElementById('inputs').innerHTML = inputs
-    console.log(inputs.length);
-}
+// function addInput() {
+//     inputs = document.getElementById('input').value;
+//     document.getElementById('inputs').innerHTML = inputs;
+//     console.log(inputs.length);
+// }
 function createNewElement2() {
     var txtNewInputBox2 = document.createElement('div');
-    txtNewInputBox.innerHTML = "<input type='text' id='newInputBox2'>";
+    txtNewInputBox2.innerHTML = "<input type='text' id='newInputBox2'>";
     document.getElementById("newElementId2").appendChild(txtNewInputBox2);
 }
 function createNewElement3() {
     var txtNewInputBox3 = document.createElement('div');
-    txtNewInputBox.innerHTML = "<input type='text' id='newInputBox3'>";
+    txtNewInputBox3.innerHTML = "<input type='text' id='newInputBox3'>";
     document.getElementById("newElementId3").appendChild(txtNewInputBox3);
 }
 
 let inputs = [];
+var x = 0;
+var array = Array();
+
+function addInput()
+{
+ array[x] = document.getElementById("input").value;
+//  alert("Element: " + array[x] + " Added at index " + x);
+ x++;
+ document.getElementById("input").value = "";
+ var e = "<hr/>";   
+    
+   for (var y=0; y<array.length; y++)
+   {
+     e += "Element " + y + " = " + array[y] + "<br/>";
+   }
+   document.getElementById("inputs").innerHTML = e;
+}
