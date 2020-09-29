acd8b26a59cfcde1975953fa311b9aeb98be8313
rabhyasighat <66615638+rabhyasighat@users.noreply.github.com>
2020-09-30 03:01:00 +0530

    3:00 am

---
+++ FLA WEB/nfa.js
@@ -0,0 +1,74 @@
+let normalInput;
+let normalStates = [];
+
+function addNormal() {
+    document.getElementById("normalstate").pattern = "[a-zA-Z]+";
+    normalInput = document.getElementById('normalstate').value;
+    let newnormal = normalInput.toUpperCase();
+    if (normalInput == 'A' || normalStates.includes(newnormal)) {
+        alert("Each new state has to be unique");
+    }
+    else {
+        normalStates.push(normalInput);
+        console.log(normalStates);
+    }
+
+}
+
+
+let finalInput;
+let finalStates = [];
+
+function addFinal() {
+    finalInput = document.getElementById('finalstate').value;
+    if (finalInput == 'A' || normalStates.includes(finalInput) || finalStates.includes(finalInput)) {
+        alert("Final state has to be unique");
+    }
+    else {
+        finalStates.push(finalInput);
+        console.log('Normal:');
+        console.log(normalStates);
+        console.log('Final');
+        console.log(finalStates);
+    }
+}
+
+let transitions = [];
+let transitionsCode = [];
+//A: [d]
+//A: d
+//A: e
+
+function addTransition() {
+    startValid = false;
+    endValid = false;
+    inputValid = false;
+    transitionStart = document.getElementById('transitionStartState').value;
+    transitionInput = document.getElementById('transitionInput').value;
+    transitionEnd = document.getElementById('transitionEndState').value;
+    if (!(normalStates.includes(transitionStart)) && (transitionStart != 'A')) {
+        alert('Enter a Valid Start State');
+    } else {
+        startValid = true;
+    }
+    if (!(finalStates.includes(transitionEnd)) && !(normalStates.includes(transitionStart)) && (transitionStart != 'A')) {
+        alert('Enter a Valid End State');
+    } else {
+        endValid = true;
+    }
+    if (startValid == true && endValid == true) {
+        var tryTransition = transitionStart + transitionInput;
+        console.log(tryTransition);
+        console.log(transitionsCode);
+
+        if (transitionsCode.includes(tryTransition)) {
+            alert('Transition already Used');
+        } else {
+            transitions.push(transitionStart + transitionInput + transitionEnd);
+            transitionsCode.push(transitionStart + transitionInput);
+            console.log(transitions);
+        }
+
+
+    }
+}
